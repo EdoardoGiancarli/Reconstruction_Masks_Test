633d19bbbed61fa0ead77c03e9fbdaa7adf6a241
Edoardo Giancarli <edo.giancarli@gmail.com>
2024-12-13 13:03:50 +0100

    .py scripts and tests.

---
+++ Img_Reconstruction_2D/maskpattern.py
@@ -0,0 +1,147 @@
+"""
+@Title: URA/MURA Coded Mask Pattern Simulation
+@Author: Edoardo Giancarli
+@Date: 13/12/24
+@Content:
+    - URAMaskPattern: Generates a 2D URA pattern for a coded mask camera.
+    - MURAMaskPattern: Generates a 2D MURA pattern for a coded mask camera.
+@References:
+    [1] codeapertures package, https://github.com/bpops
+    [2] E. E. Fenimore and T. M. Cannon, "Coded aperture imaging with uniformly redundant arrays," Appl. Opt. 17, 337-347 (1978)
+    [3] E. E. Fenimore and S. R. Gottesman, "New family of binary arrays for coded aperture imaging" Appl. Opt. 28 (20): 4344-4352 (1989)
+"""
+
+import collections.abc as c
+
+import numpy as np
+from sympy import isprime, primerange
+
+
+class URAMaskPattern:
+    """Generates a 2D URA pattern for a coded mask camera."""
+
+    def __init__(self, rank: int):
+        
+        self.pattern_type = 'URA'
+        self.rank = rank
+
+        self.prime_pair = self._get_prime_pair(rank)
+        C_r_i, C_s_j = self._get_pattern_root()
+
+        self.basic_pattern = self._get_basic_pattern(C_r_i, C_s_j)
+    
+
+    def _get_prime_pair(self, rank):
+
+        assert rank >= 0, f"rank must be >= 0, got rank = {rank} instead."
+
+        lim = 107
+        primes = list(primerange(2, lim))
+
+        p1, this_rank = primes[0], -1
+
+        for p2 in primes[1:]:
+
+            if (p2 - p1) == 2:
+                this_rank += 1
+                if this_rank == rank:
+                    return p2, p1
+
+            p1 = p2
+
+        raise ValueError(f"Could not find prime pairs in the range [2, {lim}] for rank = {rank}.")
+    
+    def _get_pattern_root(self):
+
+        r, s = self.prime_pair
+
+        C_r_i = np.zeros(r) - 1
+        C_s_j = np.zeros(s) - 1
+
+        for x in range(1, r):
+            C_r_i[x**2 % r] = 1
+        
+        for y in range(1, s):
+            C_s_j[y**2 % s] = 1
+        
+        return C_r_i, C_s_j
+    
+    def _get_basic_pattern(self, C_r_i, C_s_j):
+
+        A = np.zeros(self.prime_pair)
+
+        for i in range(self.prime_pair[0]):
+            for j in range(self.prime_pair[1]):
+
+                if i == 0: A[i,j] = 0
+
+                elif j == 0: A[i,j] = 1
+
+                elif C_r_i[i]*C_s_j[j] == 1: A[i,j] = 1
+
+                else: A[i,j] = 0
+        
+        return A
+
+
+
+class MURAMaskPattern:
+    """Generates a 2D MURA pattern for a coded mask camera."""
+
+    def __init__(self, rank: int):
+        
+        self.pattern_type = 'MURA'
+        self.rank = rank
+
+        self.l = self._get_prime(rank)
+        C_r_i, C_s_j = self._get_pattern_root()
+
+        self.basic_pattern = self._get_basic_pattern(C_r_i, C_s_j)
+    
+
+    def _get_prime(self, rank):
+
+        assert rank >= 0, f"rank must be >= 0, got rank {rank} instead."
+        
+        m, this_rank = 1, -1
+
+        while True:
+            l = 4*m + 1
+
+            if isprime(l):
+                this_rank += 1
+                if this_rank == rank:
+                    return l
+            
+            m += 1
+    
+    def _get_pattern_root(self):
+
+        C_r_i = np.zeros(self.l) - 1
+        C_s_j = np.zeros(self.l) - 1
+
+        for x in range(1, self.l):
+            C_r_i[x**2 % self.l] = 1
+        
+        for y in range(1, self.l):
+            C_s_j[y**2 % self.l] = 1
+        
+        return C_r_i, C_s_j
+    
+    def _get_basic_pattern(self, C_r_i, C_s_j):
+
+        A = np.zeros((self.l, self.l))
+
+        for i in range(self.l):
+            for j in range(self.l):
+
+                if i == 0: A[i,j] = 0
+
+                elif j == 0: A[i,j] = 1
+
+                elif C_r_i[i]*C_s_j[j] == 1: A[i,j] = 1
+        
+        return np.transpose(A)
+
+
+# end
---
+++ Img_Reconstruction_2D/test_maskpattern.py
@@ -0,0 +1,105 @@
+"""
+@Title: Test for URA/MURA Coded Mask Pattern Simulation
+@Author: Edoardo Giancarli
+@Date: 13/12/24
+@Content:
+    - TestURAMaskPattern: Tests the URAMaskPattern class in maskpattern.py.
+    - TestMURAMaskPattern: Tests the MURAMaskPattern class in maskpattern.py.
+"""
+
+import unittest
+import numpy as np
+
+from maskpattern import URAMaskPattern, MURAMaskPattern
+
+
+class TestURAMaskPattern(unittest.TestCase):
+    """Tests the URAMaskPattern class in maskpattern.py."""
+
+    def setUp(self):
+        self.ura = URAMaskPattern(rank=0)
+    
+    def test_initialization(self):
+        self.assertEqual(self.ura.pattern_type, "URA")
+        self.assertEqual(self.ura.rank, 0)
+        self.assertEqual(self.ura.prime_pair, (5, 3))
+    
+    def test_get_prime_pair(self):
+        self.assertEqual(self.ura._get_prime_pair(0), (5, 3))
+        self.assertEqual(self.ura._get_prime_pair(1), (7, 5))
+        self.assertEqual(self.ura._get_prime_pair(2), (13, 11))
+
+        with self.assertRaises(AssertionError):
+            URAMaskPattern(rank=-4)
+            self.ura._get_prime_pair(-3)
+        
+        with self.assertRaises(ValueError):
+            self.ura._get_prime_pair(100)
+    
+    def test_get_pattern_root(self):
+        C_r_i, C_s_j = self.ura._get_pattern_root()
+        C_r_i2, C_s_j2 = URAMaskPattern(rank=2)._get_pattern_root()
+
+        np.testing.assert_array_equal(C_r_i, np.array([-1, 1, -1, -1, 1]), strict=False)
+        np.testing.assert_array_equal(C_s_j, np.array([-1, 1, -1]), strict=False)
+
+        np.testing.assert_array_equal(C_r_i2, np.array([-1, 1, -1, 1, 1, -1, -1, -1, -1, 1, 1, -1, 1]), strict=False)
+        np.testing.assert_array_equal(C_s_j2, np.array([-1, 1, -1, 1, 1, 1, -1, -1, -1, 1, -1]), strict=False)
+    
+    def test_basic_pattern(self):
+        self.assertEqual(self.ura.basic_pattern.shape, (5, 3))
+        self.assertEqual(self.ura.basic_pattern[0, 0], 0)
+        self.assertEqual(self.ura.basic_pattern[1, 1], 1)
+
+        basic_pattern_rank = np.linalg.matrix_rank(self.ura.basic_pattern)
+        self.assertEqual(basic_pattern_rank, 2,
+                         "The basic pattern matrix is not URA.")
+
+
+
+class TestMURAMaskPattern(unittest.TestCase):
+    """Tests the MURAMaskPattern class in maskpattern.py."""
+
+    def setUp(self):
+        self.mura = MURAMaskPattern(rank=0)
+    
+    def test_initialization(self):
+        self.assertEqual(self.mura.pattern_type, "MURA")
+        self.assertEqual(self.mura.rank, 0)
+        self.assertEqual(self.mura.l, 5)
+    
+    def test_get_prime(self):
+        self.assertEqual(self.mura._get_prime(0), 5)
+        self.assertEqual(self.mura._get_prime(1), 13)
+        self.assertEqual(self.mura._get_prime(2), 17)
+
+        with self.assertRaises(AssertionError):
+            MURAMaskPattern(rank=-4)
+            self.mura._get_prime(-3)
+    
+    def test_get_pattern_root(self):
+        C_r_i, C_s_j = self.mura._get_pattern_root()
+        C_r_i2, C_s_j2 = MURAMaskPattern(rank=2)._get_pattern_root()
+
+        np.testing.assert_array_equal(C_r_i, C_s_j, strict=False)
+        np.testing.assert_array_equal(C_s_j, np.array([-1, 1, -1, -1, 1]), strict=False)
+
+        np.testing.assert_array_equal(C_r_i2, C_s_j2, strict=False)
+        np.testing.assert_array_equal(C_s_j2, np.array([-1, 1, 1, -1, 1, -1, -1, -1, 1, 1, -1, -1, -1, 1, -1, 1, 1]), strict=False)
+    
+    def test_basic_pattern(self):
+        self.assertEqual(self.mura.basic_pattern.shape, (5, 5))
+        self.assertEqual(self.mura.basic_pattern[0, 0], 0)
+        self.assertEqual(self.mura.basic_pattern[1, 1], 1)
+
+        basic_pattern_rank = np.linalg.matrix_rank(self.mura.basic_pattern)
+        self.assertEqual(basic_pattern_rank, 2,
+                         "The basic pattern matrix is not MURA.")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+# end
